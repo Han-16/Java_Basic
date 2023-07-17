5481e9849e576df708184ac5c302d8ca309e86b2
Han-16 <haanbk16@gmail.com>
2023-07-17 17:31:25 +0900

    DFS

---
+++ algorithm/src/DFS/DFS_Recursion.java
@@ -0,0 +1,20 @@
+package DFS;
+
+public class DFS_Recursion {
+    static int[][] graph = {{}, {2,3,8}, {1,6,8}, {1,5}, {5,7}, {3,4,7}, {2}, {4,5}, {1,2}};
+    static boolean[] visited = new boolean[graph.length];
+    public static void main(String[] args) {
+        dfs(1);
+    }
+
+    static void dfs(int nodeIndex) {
+        visited[nodeIndex] = true;
+        System.out.print(nodeIndex + " -> ");
+
+        for (int node : graph[nodeIndex]) {
+            if (!visited[node]) {
+                dfs(node);
+            }
+        }
+    }
+}
---
+++ algorithm/src/DFS/DFS_Stack.java
@@ -0,0 +1,35 @@
+package DFS;
+
+import java.util.Stack;
+
+public class DFS_Stack {
+
+    public static void main(String[] args) {
+        int[][] graph = {{}, {2,3,8}, {1,6,8}, {1,5}, {5,7}, {3,4,7}, {2}, {4,5}, {1,2}};
+        int start = 1;
+        System.out.println(bfs(start, graph));
+    }
+
+    public static String bfs(int start, int[][] graph) {
+        StringBuffer sb = new StringBuffer();
+        boolean[] visited = new boolean[graph.length];
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(start);
+        visited[start] = true;
+
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+            sb.append(node + " ");
+
+            for (int linkedNode : graph[node]) {
+                if (!visited[linkedNode]) {
+                    stack.push(linkedNode);
+                    visited[linkedNode] = true;
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
