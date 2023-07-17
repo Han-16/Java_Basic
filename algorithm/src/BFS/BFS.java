ad10d8bd5c114b86f60a2eefbe2e85a44527efef
Han-16 <haanbk16@gmail.com>
2023-07-17 17:08:03 +0900

    BFS

---
+++ algorithm/src/BFS/BFS.java
@@ -0,0 +1,68 @@
+package BFS;
+import java.util.*;
+
+public class BFS {
+    public static void main(String[] args) {
+        int[][] graph = {{}, {2, 3, 8}, {1, 6, 8},{1, 5}, {5, 7}, {3, 4, 7}, {2}, {4, 5}, {1, 2}};
+
+        boolean[] visited = new boolean[graph.length];
+
+        System.out.println(bfs(1, graph));
+    }
+
+    static String bfs(int start, int[][] graph) {
+        StringBuffer sb = new StringBuffer();
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[graph.length];
+
+        q.offer(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int nodeIndex = q.poll();
+            sb.append(nodeIndex + " -> ");
+
+            for (int i = 0; i < graph[nodeIndex].length; i++) {
+                int node = graph[nodeIndex][i];
+                if (!visited[node]) {
+                    q.offer(node);
+                    visited[node] = true;
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    static String bfs(int start, int[][] graph, boolean[] visited) {
+//        StringBuffer sb = new StringBuffer();
+//        Queue<Integer> q = new LinkedList<>();
+//
+//        q.offer(start);
+//        visited[start] = true;
+//
+//        while (!q.isEmpty()) {
+//            int nodeIndex = q.poll();
+//            sb.append(nodeIndex + " -> ");
+//            for (int i = 0; i < graph[nodeIndex].length; i++) {
+//                int temp = graph[nodeIndex][i];
+//                if (!visited[temp]) {
+//                    visited[temp] = true;
+//                    q.offer(temp);
+//                }
+//            }
+//        }
+//        return sb.toString();
