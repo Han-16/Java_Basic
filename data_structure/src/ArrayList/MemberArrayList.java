5baf90a098302edc6f7dcfcb6e67b6fb085c8176
Han-16 <haanbk16@gmail.com>
2023-07-17 16:33:52 +0900

    Iterator

--- data_structure/src/ArrayList/MemberArrayList.java
+++ data_structure/src/ArrayList/MemberArrayList.java
@@ -13,15 +13,26 @@ public class MemberArrayList {
     }
 
     public boolean removeMember(int memberId) {
-        for (int i = 0; i < arrayList.size(); i++) {
-            Member member = arrayList.get(i);
+//        for (int i = 0; i < arrayList.size(); i++) {
+//            Member member = arrayList.get(i);
+//            int tempId = member.getMemberId();
+//            if (tempId == memberId) {
+//                arrayList.remove(i);
+//                return true;
+//            }
+//        }
+//        System.out.println(memberId + "is not exist");
+//        return false;
+        Iterator<Member> ir = arrayList.iterator();
+        while (ir.hasNext()) {
+            Member member = ir.next();
             int tempId = member.getMemberId();
             if (tempId == memberId) {
-                arrayList.remove(i);
+                arrayList.remove(member);
                 return true;
             }
         }
-        System.out.println(memberId + "is not exist");
+        System.out.println(memberId + "isn't exist");
         return false;
     }
 
