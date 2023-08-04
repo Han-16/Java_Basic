ec8cdab843acb0b0902fbb40cfb8b4fa00f113e4
Han-16 <haanbk16@gmail.com>
2023-08-04 23:16:00 +0900

    try-catch

---
+++ VariousFeatures/src/TryCatch/FileExceptionHandling.java
@@ -0,0 +1,29 @@
+package TryCatch;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileExceptionHandling {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream("a.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            return;
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("finally");
+        }
+        System.out.println("end");
+
+    }
+}
