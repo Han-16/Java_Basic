9817ebb578754e2b1d8ad2c3b095b72615342ffd
Han-16 <haanbk16@gmail.com>
2023-08-04 23:35:39 +0900

    AutoClose

---
+++ VariousFeatures/src/TryCatch/AutoCloseTest.java
@@ -0,0 +1,14 @@
+package TryCatch;
+
+public class AutoCloseTest {
+    public static void main(String[] args) {
+        AutoCloseable obj = new AutoCloseableObj();
+        try(obj) {
+            throw new Exception();
+        } catch(Exception e) {
+            System.out.println("exception");
+        }
+
+        System.out.println("end!");
+    }
+}
